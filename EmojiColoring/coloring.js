aa73601fbd3b2aa3712de0a88dc802251edc5174
coder350Colin <cgupta24@nscds.org>
2019-12-17 19:09:02 +0000

    i started the custom emoji -colin

---
+++ EmojiColoring/coloring.js
@@ -0,0 +1,12 @@
+$(".pupilImage").click(
+    function(){
+        alert("I wish it was Friday");
+    }
+);
+
+$(".mouthImage").click(
+    function(){
+        alert("I am an epic gamer");
+        var clickMouth = $(this).css("background-image");
+    }
+);
